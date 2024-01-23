98d61012f6286f5a0da36d006d195572d5fa824e
Jay <jezierski.and@gmail.com>
2024-01-23 21:08:28 +0100

    cleaning

---
+++ Runtime/Standard/Equestion/CurveEquation/Editor/CurveEquationPropertyDrawer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+using And.Editor;
+
+namespace And.Math.Standard.Editor
+{
+
+    [CustomPropertyDrawer(typeof(CurveEquation))]
+    public class CurveEquationPropertyDrawer : PropertyGridDrawer
+    {
+        protected override int GetRowCount(SerializedProperty property, GUIContent label)
+        {
+            return 2;
+        }
+
+        protected override int GetColumnCount(SerializedProperty property, GUIContent label)
+        {
+            return 2;
+        }
+
+        protected override void DrawGUI(Rect pos, SerializedProperty prop, GUIContent label, PropertyGrid grid)
+        {
+            SerializedGridProperty curve = new SerializedGridProperty(prop.FindPropertyRelative(nameof(CurveEquation.curve)), 0);
+            SerializedGridProperty amplitude = new SerializedGridProperty(prop.FindPropertyRelative(nameof(CurveEquation.amplitude)), 65);
+            SerializedGridProperty length = new SerializedGridProperty(prop.FindPropertyRelative(nameof(CurveEquation.length)), 55);
+
+            GridPosition gridPos;
+
+            gridPos = new GridPosition(new Vector2Int(0, 0), new Vector2Int(2, 1));
+            curve.DrawCurve(grid, gridPos, showLabel: false);
+            gridPos = new GridPosition(new Vector2Int(0, 1));
+            amplitude.DrawFloat(grid, gridPos, showLabel: true);
+            gridPos = new GridPosition(new Vector2Int(1, 1));
+            length.DrawFloat(grid, gridPos, showLabel: true);
+        }
+    }
+
+}
--- Runtime/Standard/Range/Editor/RangePropertyDrawer.cs
+++ Runtime/Standard/Range/Editor/RangePropertyDrawer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using And.Editor;
 
 namespace And.Math.Standard.Editor
 {
@@ -7,28 +8,41 @@ namespace And.Math.Standard.Editor
     [CustomPropertyDrawer(typeof(Range))]
     public class RangePropertyDrawer : PropertyGridDrawer
     {
-        protected override int ColumnCountWrap => ColumnCountStandard - 1;
-        protected override int RowCountStandard => 1;
-        protected override int ColumnCountStandard => 3;
-        protected override bool IsWrappingSupported => true;
-        protected override bool ForceIndent => false;
-
-        protected override void DrawStandardProperties(Rect pos, SerializedProperty prop, GUIContent label, Grid grid)
+        protected override int GetRowCount(SerializedProperty property, GUIContent label)
         {
-            int labelWidth = 28;
-            SerializedGridProperty min = new SerializedGridProperty(prop.FindPropertyRelative(nameof(Range.min)), labelWidth);
-            SerializedGridProperty max = new SerializedGridProperty(prop.FindPropertyRelative(nameof(Range.max)), labelWidth);
-            GridPosition gridPos;
+            int rowCount = 1;
 
-            gridPos = new GridPosition(0, 0);
-            min.DrawFloat(grid, gridPos);
-            gridPos = new GridPosition(1, 0);
-            max.DrawFloat(grid, gridPos);
+            if(!IsFullyNested(property, label) && IsWrapping)
+                rowCount++;            
+
+            return rowCount;
         }
 
-        protected override void DrawIndentProperties(Rect pos, SerializedProperty property, GUIContent label, Grid grid)
+        protected override int GetColumnCount(SerializedProperty property, GUIContent label)
         {
-            DrawStandardProperties(pos, property, label, grid);
+            int columnCount = 3;
+
+            if (IsFullyNested(property, label) || IsWrapping)
+                columnCount--;
+
+            return columnCount;
+        }
+
+        protected override void DrawGUI(Rect pos, SerializedProperty prop, GUIContent label, PropertyGrid grid)
+        {
+            int labelWidth = 38;
+            int startRow = 0;
+            if(!IsFullyNested(prop,label) && IsWrapping)
+                startRow++;
+            SerializedGridProperty min = new SerializedGridProperty(prop.FindPropertyRelative(nameof(Range.min)), labelWidth);
+            SerializedGridProperty max = new SerializedGridProperty(prop.FindPropertyRelative(nameof(Range.max)), labelWidth);
+            
+            GridPosition gridPos;
+
+            gridPos = new GridPosition(new Vector2Int(0, startRow), new Vector2Int(1,1));
+            min.DrawFloat(grid, gridPos,showLabel: true);
+            gridPos = new GridPosition(new Vector2Int(1, startRow));
+            max.DrawFloat(grid, gridPos, showLabel: true);
         }
     }
 
